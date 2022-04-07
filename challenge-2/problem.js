74703b87d66079d13db363dd32d24f59cc2f7b44
Robson Alves <robson.alves@loft.com.br>
2022-04-07 14:13:42 -0300

    add code challenge #2

---
+++ challenge-2/problem.js
@@ -0,0 +1,16 @@
+/**
+ * Desafio
+ * Identificar o output de anotherNumber
+ */
+
+const number = 0;
+
+const anotherNumber = number || 10;
+
+console.log(anotherNumber);
+
+// O output será 0 ou 10?
+
+
+// O output será 10 pq o operador OR considera 0 como FALSO
+// Assista o vídeo para saber mais  
---
+++ challenge-2/solution.js
@@ -0,0 +1,37 @@
+const number = 0;
+
+const anotherNumber = number ?? 10; // Nullish coalescing operator
+
+console.log(anotherNumber);
+
+
+// O operador Nullish coalescing considera apenas os valores NULL e UNDEFINED como FALSO
+
+
+// Falsy Values e Truthy Values comentados no vídeo
+
+/**
+ *  Falsy Values
+ *  Valores considerados falso em uma condição
+*/
+
+//  false
+//  ''
+//  null
+//  undefined 
+
+// link para a lista completa - https://developer.mozilla.org/en-US/docs/Glossary/Falsy
+
+/**
+ *  Truthy Values
+ *  Valores considerados verdadeiro em uma condição
+*/
+
+// true
+// []
+// {}
+// 40
+// "Robson"
+
+
+// link para a lista completa - https://developer.mozilla.org/en-US/docs/Glossary/Truthy
